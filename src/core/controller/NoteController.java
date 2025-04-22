53b6070f753d10eb7fd973ba05651f5d6c3b34f3
Alexis <phucni2004@gmail.com>
2025-04-22 12:12:12 +0700

    re-setup NoteController

--- src/core/controller/NoteController.java
+++ src/core/controller/NoteController.java
@@ -2,12 +2,15 @@ package core.controller;
 
 import core.model.NoteModel;
 import core.view.NoteView;
+import core.datalayer.NoteDatabase;
 
 public class NoteController {
     private NoteModel model;
     private NoteView view;
+    private NoteDatabase database;
 
-    public NoteController(NoteModel model, NoteView view) {
+    public NoteController(NoteModel model, NoteView view, NoteDatabase database) {
+        this.database = database;
         this.model = model;
         this.view = view;
 
