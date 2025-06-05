d1a6c18354dd452ef30fc584d20e153d3ecbe4b0
Quin-Giao <giaovo11@gmail.com>
2025-06-05 20:54:48 +0700

    Update Sorter.java

--- src/core/modelservice/Sorter.java
+++ src/core/modelservice/Sorter.java
@@ -80,15 +80,60 @@ private static void merge(List<NoteModel> list, int left, int mid, int right) {
 
     while (i < leftPart.size()) list.set(k++, leftPart.get(i++));
     while (j < rightPart.size()) list.set(k++, rightPart.get(j++));
+   }
+
+
+
+private static List<NoteModel> dateSort(List<NoteModel> displayedNotes) {
+    List<NoteModel> result = new ArrayList<>(displayedNotes);
+    int n = result.size();
+
+    // Chạy insertion sort trên từng đoạn nhỏ (MIN_RUN)
+    for (int i = 0; i < n; i += MIN_RUN) {
+        insertionSortByDate(result, i, Math.min(i + MIN_RUN - 1, n - 1));
+    }
+
+    // Merge các đoạn đã sort
+    for (int size = MIN_RUN; size < n; size = 2 * size) {
+        for (int left = 0; left < n; left += 2 * size) {
+            int mid = left + size - 1;
+            int right = Math.min(left + 2 * size - 1, n - 1);
+
+            if (mid < right) {
+                mergeByDate(result, left, mid, right);
+            }
+        }
+    }
+    return result;
+}
+private static void insertionSortByDate(List<NoteModel> list, int left, int right) {
+    for (int i = left + 1; i <= right; i++) {
+        NoteModel temp = list.get(i);
+        int j = i - 1;
+        while (j >= left && list.get(j).getCreatedDateTime().compareTo(temp.getCreatedDateTime()) > 0) {
+            list.set(j + 1, list.get(j));
+            j--;
+        }
+        list.set(j + 1, temp);
+    }
 }
 
+private static void mergeByDate(List<NoteModel> list, int left, int mid, int right) {
+    List<NoteModel> leftPart = new ArrayList<>(list.subList(left, mid + 1));
+    List<NoteModel> rightPart = new ArrayList<>(list.subList(mid + 1, right + 1));
 
-
-    private static List<NoteModel> dateSort(List<NoteModel> displayedNotes){
-        List<NoteModel> result = new ArrayList<>(displayedNotes);
-        result.sort((n1, n2) -> n2.getCreatedDateTime().compareTo(n1.getCreatedDateTime())); // newest to oldest
-        return result;
+    int i = 0, j = 0, k = left;
+    while (i < leftPart.size() && j < rightPart.size()) {
+        if (leftPart.get(i).getCreatedDateTime().compareTo(rightPart.get(j).getCreatedDateTime()) <= 0) {
+            list.set(k++, leftPart.get(i++));
+        } else {
+            list.set(k++, rightPart.get(j++));
+        }
     }
+    while (i < leftPart.size()) list.set(k++, leftPart.get(i++));
+    while (j < rightPart.size()) list.set(k++, rightPart.get(j++));
+}
+
     
 
 
